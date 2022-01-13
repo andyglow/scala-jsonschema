b6ed77755637024644e79c407ffed552c2836aef
Scala Steward <me@scala-steward.org>
2022-01-13 06:13:11 +0100

    Update scala-library, scala-reflect to 2.13.8

--- project/ScalaVer.scala
+++ project/ScalaVer.scala
@@ -9,7 +9,7 @@ object ScalaVer {
 
   case object _212 extends ScalaVer("2.12.15")
 
-  case object _213 extends ScalaVer("2.13.7")
+  case object _213 extends ScalaVer("2.13.8")
 
   val values: Seq[ScalaVer] = Set(_213, _212, _211).toSeq
 
